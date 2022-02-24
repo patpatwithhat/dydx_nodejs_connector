7f5036f02550ff7f241b071a20fd465c46a6a8a5
Patrick Küsters <kuesters.patrick@gmail.com>
2022-02-24 10:49:31 +0100

    ref

---
+++ dist/dydx-connector.d.ts
@@ -0,0 +1,50 @@
+import { DydxClient, ApiKeyCredentials, PositionResponseObject, OrderResponseObject, Market, TimeInForce, OrderType, OrderSide, OrderStatus, PositionStatus, ActiveOrderResponseObject, UserResponseObject, AccountResponseObject } from "@dydxprotocol/v3-client";
+import { KeyPairWithYCoordinate } from '@dydxprotocol/starkex-lib';
+export declare const enum NetworkID {
+    MainNet = 1,
+    RopstenTestNet = 3
+}
+export declare class DYDXConnector {
+    static _client: DydxClient;
+    _positionID: string;
+    private constructor();
+    get client(): DydxClient;
+    get positionID(): string;
+    set positionID(id: string);
+    static build(networkId?: NetworkID): Promise<DYDXConnector>;
+    static checkGeneralKeysPresent(): boolean;
+    private static checkIfAllRequiredKeysPresent;
+    private static checkIfEthPrivateKeyPresent;
+    getPNLInPercent(position: PositionResponseObject): Number;
+    /**
+     *
+     * @param side BUY, SELL
+     * @param type  LIMIT, MARKET, STOP_LIMIT, TRAILING_STOP, TAKE_PROFIT
+     * @param timeInForce GTT, FOK, IOC"
+     * @param postOnly best to keep at false, dunno what this is doing
+     * @param size of token
+     * @param price you want to buy for
+     * @param limitFee
+     * @param expiration date of your order
+     * @param market DydxMarketEnum e.g. BTC_USD
+     * @returns Promise<dydx.OrderResponseObject>
+     */
+    createOrder(side: OrderSide, type: OrderType, timeInForce: TimeInForce, postOnly: boolean | undefined, size: string, price: string, limitFee: string | undefined, expiration: string | undefined, market: Market): Promise<OrderResponseObject>;
+    getPositions(market: Market, status: PositionStatus): Promise<PositionResponseObject[]>;
+    getOrders(market: Market, status: OrderStatus | undefined, side: OrderSide, type: OrderType, limit?: number): Promise<OrderResponseObject[]>;
+    getOrderById(id: string): Promise<OrderResponseObject>;
+    getActiveOrder(market: Market): Promise<ActiveOrderResponseObject[]>;
+    recoverInfo(): Promise<{
+        starkKey: string;
+    }>;
+    registerNewUser(keyPair: KeyPairWithYCoordinate): Promise<{
+        apiKey: ApiKeyCredentials;
+        user: UserResponseObject;
+        account: AccountResponseObject;
+    }>;
+    static getApiCredentials(): Promise<ApiKeyCredentials>;
+    static getKeyPairWithYCoordinate(): Promise<KeyPairWithYCoordinate>;
+    updateUser(email: string, username: string, country: string): Promise<UserResponseObject>;
+    getAccount(): Promise<AccountResponseObject>;
+    getUser(): Promise<UserResponseObject>;
+}
--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,5 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const dydx_connector_1 = require("./dydx-connector");
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        let con = yield dydx_connector_1.DYDXConnector.build(3 /* RopstenTestNet */);
-        console.log(yield con.getAccount());
-    });
-}
-main();
+exports.DYDXConnector = void 0;
+var dydx_connector_1 = require("./dydx-connector");
+Object.defineProperty(exports, "DYDXConnector", { enumerable: true, get: function () { return dydx_connector_1.DYDXConnector; } });
